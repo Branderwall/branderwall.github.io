d0854df3bcda38359fc6858ecf7927a8ef633594
Branderwall <tttgkm@gmail.com>
2020-01-11 21:32:06 -0800

    update

--- js/date.js
+++ js/date.js
@@ -1,36 +1,5 @@
-// let months = [
-//     "January",
-//     "February",
-//     "March",
-//     "April",
-//     "May",
-//     "June",
-//     "July",
-//     "August",
-//     "September",
-//     "October",
-//     "November",
-//     "December"
-// ]
+let currentDate = new Date();
+let yearNum = currentDate.getFullYear();
 
-// let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday"
-// ]
-
-// let currentDate = new Date();
-// let dayName = days[currentDate.getDay()];
-// let monthName = months[currentDate.getMonth()];
-// let dayNum = currentDate.getDate();
-// let yearNum = currentDate.getFullYear();
-
-// let fullDate = dayName + ", " + monthName + " " + dayNum + ", " + yearNum;
-
-// let lastUpdate = document.lastModified;
-
+document.getElementById("currentyear").textContent = yearNum;
 document.getElementById("lastupdated").textContent = document.lastModified;
