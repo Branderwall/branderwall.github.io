14cd522c83ecc00812137e654ca93fcef80f2005
Branderwall <tttgkm@gmail.com>
2020-02-07 17:31:21 -0800

    L5 promo

--- lesson5/js/promo.js
+++ lesson5/js/promo.js
@@ -1,6 +1,6 @@
 let now = new Date();
 let currentDay = now.getDay();
 
-if( currentDay !== 3) {
+if( currentDay !== 5) {
     document.getElementsByClassName("promo")[0].classList.add("hidden");
 }
