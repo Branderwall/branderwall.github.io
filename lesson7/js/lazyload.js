6472d653c7fe430f44a12eb6b45832a74ad3caa2
Branderwall <tttgkm@gmail.com>
2020-02-19 19:13:46 -0800

    L7 init LazyLoad

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,30 @@
+// old lazy load
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+let loadImages = image => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+//Intersection Observer
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach(item => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+
+  imagesToLoad.forEach(img => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach(img => {
+    loadImages(img);
+  });
+}
