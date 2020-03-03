cc88508c7efcf774f15e58aeecefc7a346baebd9
Branderwall <tttgkm@gmail.com>
2020-03-02 20:37:00 -0800

    L9 prophet html js

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,28 +1,39 @@
 const requestURL =
   "https://branderwall.github.io/lesson9/data/latter-day-prophets.json";
 
-  console.table(["Audi", "Volvo", "Ford"]);
-
 fetch(requestURL)
   .then(function(response) {
     return response.json();
   })
   .then(function(jsonObject) {
-    console.table(jsonObject);
+    // console.table(jsonObject);
     const prophets = jsonObject["prophets"];
     for (let i = 0; i < prophets.length; i++) {
       // Add Name
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
+      let bdate = document.createElement("p");
+      let bplace = document.createElement("p");
+      let image = document.createElement("img");
+
       h2.textContent = prophets[i].name + " " + prophets[i].lastname;
-      card.appendChild(h2);
-      document.querySelector("div.cards").appendChild(card);
 
       // Add birth date
 
+      bdate.textContent = "Date of Birth: " + prophets[i].birthdate;
+
       // Add birth place
 
+      bplace.textContent = "Place of Birth: " + prophets[i].birthplace;
+
       // Add image
+
+      image.setAttribute("src", prophets[i].imageurl);
+
+      card.appendChild(h2);
+      card.appendChild(bdate);
+      card.appendChild(bplace);
+      card.appendChild(image);
+      document.querySelector("div.cards").appendChild(card);
     }
   });
-
