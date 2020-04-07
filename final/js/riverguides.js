cc46fa202a8569c95305a28e0bded98f2c2912b4
Branderwall <tttgkm@gmail.com>
2020-04-06 22:10:36 -0700

    Final river guides

---
+++ final/js/riverguides.js
@@ -0,0 +1,47 @@
+let fetchRequest = "https://branderwall.github.io/final/data/riverguides.json";
+
+fetch(fetchRequest)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (guideObject) {
+    let guides = guideObject["guides"];
+
+    let grid = document.createElement("div");
+
+    guides.forEach((guide) => {
+      let advitem = document.createElement("section");
+      let imgframe = document.createElement("div");
+      let desc = document.createElement("div");
+      let img = document.createElement("img");
+      let name = document.createElement("h2");
+      let cert = document.createElement("p");
+      let exp = document.createElement("p");
+      let email = document.createElement("p");
+      let bio = document.createElement("p");
+
+      advitem.setAttribute("class") = "advitem";
+      imgframe.setAttribute("class") = "imgframe";
+      desc.setAttribute("class") = "advdescription";
+
+      img.setAttribute("src") = `images/${guide.img}-500.jpg`;
+      img.setAttribute("alt") = guide.name;
+
+      name.textContent = guide.name;
+      cert.textContent = guide.cert;
+      exp.textContent = `${guide.exp} years experience`;
+      email.textContent = `Email: ${guide.email}`;
+      bio.textContent = guide.bio;
+
+      advitem.appendChild(imgframe);
+      imgframe.appendChild(img);
+      advitem.appendChild(desc);
+      desc.appendChild(img);
+      desc.appendChild(name);
+      desc.appendChild(cert);
+      desc.appendChild(exp);
+      desc.appendChild(bio);
+
+      document.querySelector("div#riverguides").appendChild(advitem);
+    });
+  });
---
+++ final/js/weatherapi.js
@@ -0,0 +1,30 @@
+let weatherapi = "824643210478c051f7bb4876cb2763ba";
+
+const lat = 45.417540;
+const lon = -116.317810;
+
+const weatherapiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherapi}`;
+
+fetch(weatherapiURL)
+  .then(response => response.json())
+  .then(jsObject => {
+    let status = jsObject.weather[0].description;
+    let temp = jsObject.main.temp.toFixed(0);
+    let humidity = jsObject.main.humidity;
+    let windspeed = jsObject.wind.speed;
+    let windchill =
+      temp <= 50 && windspeed > 3
+        ? (
+            35.74 +
+            0.6215 * temp -
+            35.75 * windspeed ** 0.16 +
+            0.4275 * temp * windspeed ** 0.16
+          ).toFixed(0)
+        : "N/A";
+
+    document.getElementById("status").textContent = status;
+    document.getElementById("temp").textContent = temp;
+    document.getElementById("humidity").textContent = humidity;
+    document.getElementById("windspeed").textContent = windspeed;
+    document.getElementById("windchill").textContent = windchill;
+  });
